6e47f17fa4cad27e1cfe1f051a564165963fe502
x3mart <x3mat@gmail.com>
2021-10-25 18:36:12 +0300

    rusiian application names

--- images/apps.py
+++ images/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class ImagesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'images'
+    verbose_name = _('Галерея и Лого')
 
     def ready(self):
         import images.signals
--- properties/apps.py
+++ properties/apps.py
@@ -1,9 +1,13 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
+
 
 
 class PropertiesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'properties'
+    verbose_name = _("Недвижимость")
+
 
     def ready(self):
         import properties.signals
