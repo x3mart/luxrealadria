e6ec26d6095760a7b019dc48d1e6c64555ed6c97
x3mart <x3mat@gmail.com>
2021-10-26 13:32:33 +0300

    minor fix

--- siteelements/views.py
+++ siteelements/views.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 from rest_framework.decorators import api_view
 from contacts.models import ContactItem
 from legals.models import FAQItem, LegalInfoItem
-from properties.models import Category, Property
+from properties.models import Category, Property, Status
 from properties.views import SomeData
 from siteelements.models import FAQ, Homepage, LegalInfo
 from siteelements.serializers import ContactPageSerializer, HomePageSerializer, LegalIfoPageSerializer
@@ -20,15 +20,19 @@ def get_home_page(request):
     home.subtitle = home_page.subtitle
     home.wallpaper = home_page.wallpaper
     home.categories = Category.objects.filter(is_active=True).annotate(properties_count=Count('properties', filter=Q(properties__is_active=True)))
-    home.trends = Property.objects.filter(is_trend=True).filter(is_active=True).prefetch_related('property_gallary').prefetch_related('category').prefetch_related('statuses').prefetch_related('region').prefetch_related('purpose')[:5]
-    home.recently_added = Property.objects.filter(is_active=True).prefetch_related('property_gallary').prefetch_related('category').prefetch_related('statuses').prefetch_related('region').prefetch_related('purpose').order_by('-created_at')[:5]
-    home.faq = FAQ.objects.prefetch_related('faq_items').first()
+    faq_items = FAQItem.objects.filter(is_active=True)
+    prefetch_faq_items = Prefetch('faq_items', queryset=faq_items)
+    statuses = Status.objects.filter(is_active=True)
+    prefetch_statuses = Prefetch('statuses', queryset=statuses)
+    home.trends = Property.objects.filter(is_trend=True).filter(is_active=True).prefetch_related('property_gallary').prefetch_related('category').prefetch_related(prefetch_statuses).prefetch_related('region').prefetch_related('purpose')[:5]
+    home.recently_added = Property.objects.filter(is_active=True).prefetch_related('property_gallary').prefetch_related('category').prefetch_related(prefetch_statuses).prefetch_related('region').prefetch_related('purpose').order_by('-created_at')[:5]
+    home.faq = FAQ.objects.prefetch_related(prefetch_faq_items).first()
     return Response(HomePageSerializer(home).data)
 
 @api_view(['GET',])
 def get_legal_info_page(request):
     try:
-        legal_info_items = LegalInfoItem.filter(is_active=True)
+        legal_info_items = LegalInfoItem.objects.filter(is_active=True)
         prefetch_legal_info_items = Prefetch('legal_info_items', queryset=legal_info_items)
         legal_info = LegalInfo.objects.prefetch_related(prefetch_legal_info_items).first()
         Response(LegalIfoPageSerializer(legal_info).data)
@@ -38,7 +42,7 @@ def get_legal_info_page(request):
 @api_view(['GET',])
 def get_contacts_page(request):
     try:
-        contact_items = ContactItem.filter(is_active=True)
+        contact_items = ContactItem.objects.filter(is_active=True)
         prefetch_contact_items = Prefetch('contact_items', queryset=contact_items)
         contact = LegalInfo.objects.prefetch_related(prefetch_contact_items).first()
         Response(ContactPageSerializer(contact).data)
