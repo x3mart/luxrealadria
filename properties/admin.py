23c7121321e6d69ba5d22edad3895529d2e28086
x3mart <x3mat@gmail.com>
2021-10-27 18:47:12 +0300

    danger color for inactive region, category, purpose in properties list

--- properties/admin.py
+++ properties/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from images.models import PropertyImage
 from django.utils.safestring import mark_safe
+from django.utils.html import format_html
 from properties.models import Category, Feature, Property, Purpose, Region, Status
 
 # Register your models here.
@@ -19,12 +20,39 @@ class PropertyImageInline(admin.TabularInline):
     get_photo.short_description = 'Миниатюра'
 
 class PropertyAdmin(admin.ModelAdmin):
-    list_display = ('name', 'is_active', 'is_trend', 'region', 'purpose', 'category')
+    list_display = ('name', 'is_active', 'is_trend', 'region_colored', 'purpose_colored', 'category_colored')
     list_editable = ('is_active', 'is_trend')
     readonly_fields = ('unique_id',)
     inlines = [
         PropertyImageInline,
     ]
+
+    @admin.display(description='Регион')
+    def region_colored(self, obj):
+        if not obj.region.is_active:
+            return format_html(
+                '<span style="color: red">{} (не активен) </span>',
+                obj.region.title
+            )
+        return obj.region.title
+    
+    @admin.display(description='Назначение')
+    def purpose_colored(self, obj):
+        if not obj.purpose.is_active:
+            return format_html(
+                '<span style="color: red">{} (не активно) </span>',
+                obj.purpose.title
+            )
+        return obj.purpose.title
+    
+    @admin.display(description='Категория')
+    def category_colored(self, obj):
+        if not obj.category.is_active:
+            return format_html(
+                '<span style="color: red">{} (не активна) </span>',
+                obj.category.title
+            )
+        return obj.category.title
 
 
 class PRSFAdmin(admin.ModelAdmin):
