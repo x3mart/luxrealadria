9aab9a8232ad1aebedba535dd21e0070a3806e4b
x3mart <x3mat@gmail.com>
2021-10-27 15:49:15 +0300

    minor fix

--- images/serializers.py
+++ images/serializers.py
@@ -3,9 +3,16 @@ from images.models import Logo, PropertyImage
 
 
 class PropertyGallarySerializer(serializers.ModelSerializer):
+    tmb_image = serializers.SerializerMethodField(read_only=True)
     class Meta:
         model = PropertyImage
         fields =  '__all__'
+    
+    def get_tmb_image(self, obj):
+        if obj.tmb_image:
+            request = self.context.get('request')
+            return request.build_absolute_uri(obj.tmb_image) 
+        return None 
 
 class LogoSerializer(serializers.ModelSerializer):
     class Meta:
