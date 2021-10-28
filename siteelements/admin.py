efe62d02d7d4598ea2b693870030f724fdbb6f94
x3mart <x3mat@gmail.com>
2021-10-28 21:54:51 +0300

    usefull article page

--- siteelements/admin.py
+++ siteelements/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from siteelements.models import FAQ, Homepage, LegalInfo, SiteLogo, Contact
+from siteelements.models import FAQ, Homepage, LegalInfo, SiteLogo, Contact, UsefullArticle
 
 # Register your models here.
 admin.site.register(LegalInfo)
@@ -8,3 +8,4 @@ admin.site.register(SiteLogo)
 admin.site.register(FAQ)
 admin.site.register(Contact)
 admin.site.register(Homepage)
+admin.site.register(UsefullArticle)
--- siteelements/urls.py
+++ siteelements/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import path
 
-from siteelements.views import get_home_page, get_legal_info_page, get_contacts_page
+from siteelements.views import get_home_page, get_legal_info_page, get_contacts_page, get_usefull_articles_page
 
 # router = DefaultRouter()
 # router.register(r'categories', CategoryViewSet, basename='category')
@@ -10,5 +10,6 @@ urlpatterns = [
             path('homepage/', get_home_page, name='homepage'),
             path('legalinfo/', get_legal_info_page, name='legalinfo'),
             path('contacts/', get_contacts_page, name='contact'),
+            path('usefullarticles/', get_usefull_articles_page, name='usefullarticle'),
             ]
 # urlpatterns += router.urls
