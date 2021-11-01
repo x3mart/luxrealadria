04f0ca5780b88a5aff06c2853d7730c3f62936bf
x3mart <x3mat@gmail.com>
2021-11-01 16:26:58 +0300

    minor fix

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,5 @@
 from rest_framework_simplejwt.views import TokenObtainPairView
-
 from accounts.serializers import MyTokenObtainPairSerializer
 
-# Create your views here.
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
